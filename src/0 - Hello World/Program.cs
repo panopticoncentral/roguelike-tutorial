2916c3bb8da3305354fef9dae0ac0858908a1dfb
Paul Vick <paulv@microsoft.com>
2020-04-06 10:21:52 -0700

    New direction

---
+++ src/0 - Hello World/Program.cs
@@ -0,0 +1,27 @@
+﻿using Citadel;
+using Citadel.Display;
+using SdlSharp;
+
+namespace RoguelikeTutorial
+{
+    internal class Program
+    {
+        private static readonly Size s_screenSize = (1280, 720);
+
+        private static void Main()
+        {
+            using var game = new Game(new[]
+            {
+                new DisplayConfiguration(
+                    title: "RougelikeTutorial",
+                    size: s_screenSize
+                )
+            });
+
+            if (game.Initialize())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ src/1 - The Protagonist/Program.cs
@@ -0,0 +1,13 @@
+﻿using SdlSharp;
+
+namespace RoguelikeTutorial
+{
+    internal class Program
+    {
+        private static readonly Size s_screenSize = (1280, 720);
+
+        private static void Main()
+        {
+        }
+    }
+}
